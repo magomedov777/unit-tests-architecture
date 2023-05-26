c731866358b8b68df5280575ca2dba8aa7b9ed22
Arkadiy <alibullterrier@gmail.com>
2023-05-26 06:11:51 +0300

    remove todolist const

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -32,9 +32,7 @@ export function Todolist(props: PropsType) {
         props.addTask(title, props.id);
     }
 
-    const removeTodolist = () => {
-        props.removeTodolist(props.id);
-    }
+    
     const changeTodolistTitle = (title: string) => {
         props.changeTodolistTitle(props.id, title);
     }
